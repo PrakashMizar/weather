cebf1dad98ecf3c6a0f8d5dcd4ac8a9ee32eed41
Prakash Mizar <prakashsarkit@gmail.com>
2021-07-29 20:26:08 +0600

    geo locator
    package

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,54 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screen/loading_screen.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Weather APP using API'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'WEATHER: Get Location',
-              style: TextStyle(
-                fontFamily: 'Dancing',
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    home: LoadingScreen(),
+  ));
 }
---
+++ weather_app/lib/screen/loading_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather APP API'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.cloud),
+          ),
+        ],
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage('images/cloudy.png'),
+              colorBlendMode: BlendMode.colorDodge,
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Text(
+              'WEATHER: Get Location',
+              style: TextStyle(
+                fontFamily: 'Dancing',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.amber),
+              ),
+              onPressed: () => getLocation(),
+              child: Text(
+                'Get LatLon',
+                style: TextStyle(
+                  fontFamily: 'Dancing',
+                  color: Colors.black,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
