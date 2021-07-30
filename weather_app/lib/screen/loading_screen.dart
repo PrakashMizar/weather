35c5c897b62c2d7c4fc90481bc7fbee421c20252
Prakash Mizar <prakashsarkit@gmail.com>
2021-07-30 11:54:53 +0600

    networking file added

--- weather_app/lib/screen/loading_screen.dart
+++ weather_app/lib/screen/loading_screen.dart
@@ -3,6 +3,8 @@ import 'package:weather_app/services/location.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+const apiKey = 'acd7db633f44cc69cf09cf5825ea8a61';
+
 class LoadingScreen extends StatefulWidget {
   LoadingScreen({Key? key}) : super(key: key);
 
@@ -11,6 +13,11 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double? latt;
+  late double? long;
+  late String? wDesc = '';
+  // late double? weatherTemp;
+
   @override
   void initState() {
     super.initState();
@@ -20,33 +27,45 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longtitude);
+    latt = location.latitude;
+    long = location.longtitude;
+    getData();
   }
 
   void getData() async {
+    //'https://api.openweathermap.org/data/2.5/weather?ilat=$latt&lon=$long=2172797&appid=$apiKey'),
+    var uri =
+        'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=acd7db633f44cc69cf09cf5825ea8a61';
     http.Response response = await http.get(
-      Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=acd7db633f44cc69cf09cf5825ea8a61'),
+      Uri.parse(uri),
     );
+    //NetworkHelp nethelper = NetworkHelp(url);
+    //var weatherdata =  nethelper.getData()
     if (response.statusCode == 200) {
       String data = response.body;
-      var longitude = jsonDecode(data)['coord']['lon'];
-      var weatherDesc = jsonDecode(data)['coord'][0]['description'];
 
+      var longitude = jsonDecode(data)['coord']['lon'];
+      var weatherDesc = jsonDecode(data)['weather'][0]['description'];
+      var weatherTemp = jsonDecode(data)['main']['temp'];
+      //print in the conosole
+      wDesc = weatherDesc;
+      print(wDesc);
       print(longitude);
       print(weatherDesc);
+      print(weatherTemp);
     } else {
+      //check error code for exception handling
       print(response.statusCode);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
+    //move to getlocation())
+    //getData();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Weather APP API'),
+        title: Text('Weather APP API $wDesc '),
         actions: [
           IconButton(
             onPressed: () {},
@@ -67,9 +86,29 @@ class _LoadingScreenState extends State<LoadingScreen> {
               height: 15.0,
             ),
             Text(
-              'WEATHER: Get Location',
+              'Get weather from API $wDesc',
               style: TextStyle(
                 fontFamily: 'Dancing',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              'longitude $long',
+              style: TextStyle(
+                fontFamily: 'Dancing',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Text(
+              'Description $wDesc ',
+              style: TextStyle(
                 fontSize: 20.0,
                 fontWeight: FontWeight.bold,
                 fontStyle: FontStyle.italic,
---
+++ weather_app/lib/services/networking.dart
@@ -0,0 +1,8 @@
+//put the getdata() function
+//make NetworkHelp class with a constructor : will receive the url passed
+//define one string variable to store url
+class NetworkHelp{
+  //consturctor
+  //variable
+  //future function
+}
